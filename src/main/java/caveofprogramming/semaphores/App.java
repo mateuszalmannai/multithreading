694cc20e762c02815cb7f9c3cde44b2feac39500
mateusz <mateuszalmannai@googlemail.com>
2016-05-16 19:31:03 +0200

    semaphores

---
+++ src/main/java/caveofprogramming/semaphores/App.java
@@ -0,0 +1,20 @@
+package caveofprogramming.semaphores;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class App {
+  public static void main(String[] args) throws InterruptedException {
+
+    final ExecutorService executor = Executors.newCachedThreadPool();
+
+    for (int i = 0; i < 200; i++) {
+      executor.submit((Runnable) () -> Connection.getInstance().connect());
+    }
+
+    executor.shutdown();
+
+    executor.awaitTermination(1, TimeUnit.DAYS);
+  }
+}
