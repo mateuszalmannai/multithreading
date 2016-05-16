8f56ab9cd1ed57f010f97e378edb03550b5d42c4
mateusz <mateuszalmannai@googlemail.com>
2016-05-16 22:37:45 +0200

    swing worker

---
+++ src/main/java/caveofprogramming/swingworker/App.java
@@ -0,0 +1,9 @@
+package caveofprogramming.swingworker;
+
+import javax.swing.*;
+
+public class App {
+  public static void main(String[] args) {
+    SwingUtilities.invokeLater(() -> new MainFrame("SwingWorker Demo"));
+  }
+}
